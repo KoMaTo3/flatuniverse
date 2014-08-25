1e77f83dda011c636ac061bf88b0ac7a8148d0c0
KoMaTo3 <fanatghost@gmail.com>
2014-08-25 10:45:13 +0400

    +lua: подключен модуль coroutine

--- src/game/lua.cpp
+++ src/game/lua.cpp
@@ -218,7 +218,7 @@ void Lua::LoadLibs() {
   static const luaL_Reg loadedlibs[] = {
     {"_G", luaopen_base},
     //{LUA_LOADLIBNAME, luaopen_package},
-    //{LUA_COLIBNAME, luaopen_coroutine},
+    {LUA_COLIBNAME, luaopen_coroutine},
     {LUA_TABLIBNAME, luaopen_table},
     //{LUA_IOLIBNAME, luaopen_io},
     //{LUA_OSLIBNAME, luaopen_os},
